088e1229999facddd4da4b647edb01f7c201caa1
cultureweb <cedric.milius@yahoo.fr>
2021-01-02 21:00:39 +0100

    Home view

---
+++ src/views/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Méthode Lafay : des exercices de musculation sans matériel</h1>
+      <h2>Bienvenue</h2>
+
+      <h3>COMMENCEZ LA SEANCE</h3>
+    </div>
+  );
+};
+
+export default Home;
