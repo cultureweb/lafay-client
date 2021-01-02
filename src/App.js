3b568ef737dd456f9fe97d01c00c8fbe18d19f22
cultureweb <cedric.milius@yahoo.fr>
2021-01-02 21:17:22 +0100

    add router to switch from login to home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
 import Home from "./views/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  console.log({ isLoggedIn });
+
   return (
     <div className="App">
-      <Home />
+      <Router>
+        {!isLoggedIn ? (
+          <>
+            <Switch>
+              <Route path="/">
+                <h1> login page</h1>
+              </Route>
+            </Switch>
+          </>
+        ) : (
+          <Home />
+        )}
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
